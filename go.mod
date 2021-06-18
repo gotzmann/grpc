80089ce12c986e2689e9731c07079e08e4e4c061
Serge Gotsuliak <sgotsulyak@gmail.com>
2021-06-18 04:07:39 +0300

    SQLBoiler minimum

--- go.mod
+++ go.mod
@@ -5,6 +5,8 @@ go 1.16
 require (
 	github.com/gofrs/uuid v3.2.0+incompatible
 	github.com/golang/protobuf v1.4.2
+	github.com/volatiletech/null/v8 v8.1.2 // indirect
+	github.com/volatiletech/sqlboiler/v4 v4.6.0 // indirect
 	google.golang.org/grpc v1.36.0
 	google.golang.org/protobuf v1.25.0
 )
--- server/main.go
+++ server/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"context"
+	"fmt"
+	"github.com/gofrs/uuid"
 	"google.golang.org/protobuf/types/known/wrapperspb"
 	"log"
 	"net"
@@ -21,19 +23,19 @@ type server struct {
 }
 
 func (s *server) AddProduct(ctx context.Context, in *pb.Product) (*pb.ProductID, error) {
-//	out, err := uuid.NewV4()
-//	if err != nil {
-//		return nil, status.Errorf(codes.Internal, "Error while generating Product ID", err)
-//	}
-//	in.Id = out.String()
-	in.Id = 1980
+	out, err := uuid.NewV4()
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, "Error while generating Product ID", err)
+	}
+	in.Id = out.String()
+/////	in.Id = 1980
 //	if s.productMap == nil {
 //		s.productMap = make(map[string]*pb.Product)
 //	}
 //	s.productMap[in.Id] = in
 	log.Printf("Product %v : %v - Added.", in.Id, in.Name)
 //	return &pb.ProductID{Value: in.Id}, status.New(codes.OK, "").Err()
-	return &pb.ProductID{Value: 1980}, status.New(codes.OK, "").Err()
+	return &pb.ProductID{Value: in.Id}, status.New(codes.OK, "").Err()
 }
 
 /////func (s *server) GetProductsByBrand(ctx context.Context, in *pb.ProductID) (*pb.Product, error) {
@@ -51,6 +53,7 @@ func (s *server) GetProductsByBrand(ctx context.Context, in *wrapperspb.StringVa
 //}
 
 func main() {
+	fmt.Println("[gRPC] Server started...")
 	lis, err := net.Listen("tcp", port)
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
@@ -60,4 +63,5 @@ func main() {
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
 	}
+	fmt.Println("[gRPC] Server stopped...")
 }
