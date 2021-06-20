55913acd617cbaee1f4e236d109e0937a8ea8c97
Serge Gotsuliak <sgotsulyak@gmail.com>
2021-06-20 17:10:48 +0300

    Fix SQL scripts

--- seed/drop.sql
+++ seed/drop.sql
@@ -9,17 +9,17 @@ Version: 1.0
 */
 
 -- drop tables
-DROP TABLE IF EXISTS sales.order_items;
-DROP TABLE IF EXISTS sales.orders;
-DROP TABLE IF EXISTS production.stocks;
-DROP TABLE IF EXISTS production.products;
-DROP TABLE IF EXISTS production.categories;
-DROP TABLE IF EXISTS production.brands;
-DROP TABLE IF EXISTS sales.customers;
-DROP TABLE IF EXISTS sales.staffs;
-DROP TABLE IF EXISTS sales.stores;
+DROP TABLE IF EXISTS order_items;
+DROP TABLE IF EXISTS orders;
+DROP TABLE IF EXISTS stocks;
+DROP TABLE IF EXISTS products;
+DROP TABLE IF EXISTS categories;
+DROP TABLE IF EXISTS brands;
+DROP TABLE IF EXISTS customers;
+DROP TABLE IF EXISTS staffs;
+DROP TABLE IF EXISTS stores;
 
 -- drop the schemas
 
-DROP SCHEMA IF EXISTS sales;
-DROP SCHEMA IF EXISTS production;
+-- DROP SCHEMA IF EXISTS sales;
+-- DROP SCHEMA IF EXISTS production;
