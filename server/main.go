f6e8a97c597d60dbedfdabc92ad4f1b9568430de
Serge Gotsuliak <sgotsulyak@gmail.com>
2021-06-21 23:53:35 +0300

    SQL Boiler with Relations

--- server/main.go
+++ server/main.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"database/sql"
 	"fmt"
-	"github.com/volatiletech/sqlboiler/v4/types"
 	"log"
 	"net"
 
@@ -14,6 +13,8 @@ import (
 	"github.com/volatiletech/sqlboiler/v4/boil"
 	_ "github.com/volatiletech/sqlboiler/v4/drivers"
 	_ "github.com/volatiletech/sqlboiler/v4/drivers/sqlboiler-psql/driver"
+	. "github.com/volatiletech/sqlboiler/v4/queries/qm"
+	"github.com/volatiletech/sqlboiler/v4/types"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
@@ -34,8 +35,6 @@ func (s *server) AddProduct(ctx context.Context, in *proto.Product) (*proto.Prod
 		return nil, err
 	}
 
-	fmt.Printf("\nproto.Product\n%v+", in)
-
 	var price decimal.Big
 	price.SetFloat64(in.Price)
 
@@ -46,9 +45,6 @@ func (s *server) AddProduct(ctx context.Context, in *proto.Product) (*proto.Prod
 		ModelYear: int16(in.Year),
 		ListPrice: types.Decimal{Big: &price},
 	}
-
-	fmt.Printf("\n=== price = \n%v+", price)
-	fmt.Printf("\nmodels.Product\n%v+", product)
 
 	// Blacklist primary key to avoid duplication of IDs
 	// Repair nulled sequence counter with SQL if needed:
@@ -62,27 +58,44 @@ func (s *server) AddProduct(ctx context.Context, in *proto.Product) (*proto.Prod
 	return &proto.ProductID{Value: int32(product.ProductID)}, status.New(codes.OK, "").Err()
 }
 
-/////func (s *server) GetProductsByBrand(ctx context.Context, in *pb.ProductID) (*pb.Product, error) {
 func (s *server) GetProductsByBrand(ctx context.Context, brand *wrapperspb.StringValue) (*proto.ProductsResponse, error) {
 	db, err := sql.Open("postgres", "host=localhost port=5432 user=postgres password=password dbname=grpc sslmode=disable")
 	if err != nil {
 		return nil, err
 	}
+	boil.DebugMode = true
 
-	//products, err := models.Products(qm.Where()).All(ctx, db)
-	products, err := models.Products().All(ctx, db)
+	/* --- Dumb way of selecting with Eager Loading and looping through all collection
+	useFiltering := true
+	products, err := models.Products(
+		Load("Brand"),
+	).All(ctx, db) */
+
+	// --- Better way
+	useFiltering := false
+	products, err := models.Products(
+		InnerJoin("brands on brands.brand_id = products.brand_id"),
+		Load("Brand", Where("brands.brand_name = ?", brand.Value)),
+	).All(ctx, db)
+
 	if err != nil {
 		return nil, status.Errorf(codes.Internal, "Error while looking products", err)
 	}
 
-	if len(products) == 0 {
+	if !useFiltering && len(products) == 0 {
 		return nil, status.Errorf(codes.NotFound, "Brand %s has no products", brand.Value)
 	}
 
-	grpcProducts := make([]*proto.Product, len(products))
+	brandProducts := make([]*proto.Product, len(products))
 	for i, p := range products {
+
+		// Skip the products of other brands for the dumb version
+		if useFiltering && brand.Value != p.R.Brand.BrandName {
+			continue
+		}
+
 		price, _ := p.ListPrice.Float64() // TODO Check errors!
-		grpcProduct := proto.Product{
+		brandProduct := proto.Product{
 			Id: int32(p.ProductID),
 			Name: p.ProductName,
 			BrandId: int32(p.BrandID),
@@ -90,12 +103,16 @@ func (s *server) GetProductsByBrand(ctx context.Context, brand *wrapperspb.Strin
 			Year: int32(p.ModelYear),
 			Price: price,
 		}
-		grpcProducts[i] = &grpcProduct
+		brandProducts[i] = &brandProduct
+	}
+
+	if len(brandProducts) == 0 {
+		return nil, status.Errorf(codes.NotFound, "Brand %s has no products", brand.Value)
 	}
 
 	log.Printf("\nReturned %d products for brand %s", len(products), brand.Value)
 
-	return &proto.ProductsResponse{Products: grpcProducts}, status.New(codes.OK, "").Err()
+	return &proto.ProductsResponse{Products: brandProducts}, status.New(codes.OK, "").Err()
 }
 
 func main() {
